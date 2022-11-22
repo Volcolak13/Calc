44d863a8a8cd87605c60f2d05b6fc96ba7882d71
Volcolak13 <Volcolak@gmail.com>
2022-11-22 17:58:47 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+﻿This is a simple calculator on PyQt6.
---
+++ main.py
@@ -0,0 +1,22 @@
+﻿from PyQt6.QtCore import Qt, QSize
+from PyQt6.QtWidgets import QApplication, QWidget, QPushButton, QMainWindow
+import sys
+
+app = QApplication(sys.argv)
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("Calc")
+        button = QPushButton("Test")
+        self.setCentralWidget(button)
+        self.setFixedSize(QSize(400, 300))
+
+
+window = MainWindow()
+window.show()
+
+
+app.exec()
