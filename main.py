bf2e539f8ac8a823100a463528d97ff7ffc1510e
Greg <Volcolak@gmail.com>
2022-11-23 21:57:05 +0300

    updated

--- main.py
+++ main.py
@@ -15,14 +15,15 @@ class MainWindow(QMainWindow):
         super().__init__()
         print("MaiinWindow initialization")
         
-        # self.setFixedSize(QSize(400, 300))
+        self.setFixedSize(QSize(270, 300))
         self.setWindowTitle("Calc")
 
-        btns1 = ["C", "DEL", "=", "*"]
+        btns1 = ["C", "DEL", ".", "*"]
         btns2 = ["1", "2", "3", "/"]
         btns3 = ["4", "5", "6", "+"]
         btns4 = ["7", "8", "9", "-"]
-        btns5 = ["(", "0", ")", "X^2"]
+        btns5 = ["+/-", "0", ".", "="]
+        
 
         # self.label = QLabel()
         # self.input = QLineEdit()
@@ -30,7 +31,7 @@ class MainWindow(QMainWindow):
 
         # self.button = QPushButton("Test")
         # self.button = Buttn()
-        LCD = QLCDNumber()
+        self.LCD = QLCDNumber()
         layout1 = QVBoxLayout()
         layout2 = QHBoxLayout()
         layout3 = QHBoxLayout()
@@ -38,32 +39,47 @@ class MainWindow(QMainWindow):
         layout5 = QHBoxLayout()
         layout6 = QHBoxLayout()
         layout7 = QHBoxLayout()
+        layout8 = QHBoxLayout()
 
-        layout2.addWidget(LCD)
+        layout2.addWidget(self.LCD)
 
         for bt in btns1:
             button = QPushButton(bt)
-            button.com = bt
-            print(button.com)
+            button.setFixedSize(QSize(60, 30))
+            # button.clicked.connect(self.the_button_was_clicked)
+            button.clicked.connect(lambda: self.set_digit(bt))
             layout3.addWidget(button)
 
         for bt in btns2:
             button = QPushButton(bt)
+            button.setFixedSize(QSize(60, 30))
+            button.clicked.connect(lambda: self.set_digit(bt))
             layout4.addWidget(button)
 
         for bt in btns3:
             button = QPushButton(bt)
+            button.setFixedSize(QSize(60, 30))
+            button.clicked.connect(lambda: self.set_digit(bt))
             layout5.addWidget(button)
 
         for bt in btns4:
             button = QPushButton(bt)
+            button.setFixedSize(QSize(60, 30))
+            button.clicked.connect(lambda: self.set_digit(bt))
             layout6.addWidget(button)
         
         for bt in btns5:
             button = QPushButton(bt)
+            button.setFixedSize(QSize(60, 30))
+            button.clicked.connect(lambda: self.set_digit(bt))
             layout7.addWidget(button)
-        
 
+        btt = "111"
+        layout8.addWidget(QPushButton(btt))
+        button.setFixedSize(QSize(60, 30))
+        button.clicked.connect(lambda: self.set_digit(btt)) 
+        
+        
         # layout = QVBoxLayout()
         # layout.addWidget(self.input)
         # layout.addWidget(self.label)
@@ -77,9 +93,14 @@ class MainWindow(QMainWindow):
         layout1.addLayout(layout5)
         layout1.addLayout(layout6)
         layout1.addLayout(layout7)
+        layout1.addLayout(layout8)
         container = QWidget()
         container.setLayout(layout1)
         self.setCentralWidget(container)
+    
+    def set_digit(self, value):
+        print(value)
+        self.LCD.display(value)
     
     def the_button_was_clicked(self):
         # self.button.setText("Done")
