2b7d867d366bc104acb55fb9291e6b54efdf25a0
Volcolak13 <Volcolak@gmail.com>
2022-11-24 13:07:25 +0300

    Rebuild UI

--- main.py
+++ main.py
@@ -1,114 +1,68 @@
 ﻿from PyQt6.QtCore import QSize
-from PyQt6.QtWidgets import QApplication, QWidget, QPushButton, QMainWindow, QHBoxLayout, QVBoxLayout, QLCDNumber
+from PyQt6.QtWidgets import QApplication, QWidget, QPushButton, QLCDNumber, QGridLayout
 import sys
 
 
 class Buttn(QPushButton):
-    def __init__(self):
+    def __init__(self, name):
         super().__init__()
-        self.setText("test")
-        print("Buttn init")
+        self.name = name
+        self.setText(name)
 
 
-class MainWindow(QMainWindow):
+class MainWindow(QWidget):
     def __init__(self):
         super().__init__()
+        self.initUI()
         print("MaiinWindow initialization")
 
-        self.setFixedSize(QSize(270, 300))
-        self.setWindowTitle("Calc")
+        self.setFixedSize(QSize(270, 370))
 
-        btns1 = ["C", "DEL", ".", "*"]
-        btns2 = ["1", "2", "3", "/"]
-        btns3 = ["4", "5", "6", "+"]
-        btns4 = ["7", "8", "9", "-"]
-        btns5 = ["+/-", "0", ".", "="]
+    def initUI(self):
+        grid = QGridLayout()
+        self.setLayout(grid)
 
-        # self.label = QLabel()
-        # self.input = QLineEdit()
-        # self.input.textChanged.connect(self.label.setText)
+        names = ["C", "DEL", "", "*",
+                 "1", "2", "3", "/",
+                 "4", "5", "6", "+",
+                 "7", "8", "9", "-",
+                 "+/-", "0", ".", "="]
 
-        # self.button = QPushButton("Test")
-        # self.button = Buttn()
+        positions = [(i, j) for i in range(2, 7) for j in range(4)]
+
         self.LCD = QLCDNumber()
-        layout1 = QVBoxLayout()
-        layout2 = QHBoxLayout()
-        layout3 = QHBoxLayout()
-        layout4 = QHBoxLayout()
-        layout5 = QHBoxLayout()
-        layout6 = QHBoxLayout()
-        layout7 = QHBoxLayout()
-        layout8 = QHBoxLayout()
+        self.formula = "0"
+        self.LCD.display(self.formula)
+        self.LCD.setFixedHeight(40)
+        self.LCD.setDigitCount(12)
+        self.LCD.setDecMode()
+        grid.addWidget(self.LCD, 0, 0, 1, 4)
 
-        layout2.addWidget(self.LCD)
+        self.buttons = []
 
-        for bt in btns1:
-            button = QPushButton(bt)
-            button.setFixedSize(QSize(60, 30))
-            # button.clicked.connect(self.the_button_was_clicked)
-            button.clicked.connect(lambda: self.set_digit(bt))
-            layout3.addWidget(button)
+        for position, name in zip(positions, names):
+            if name == '':
+                continue
+            button = Buttn(name)
+            button.setFixedSize(QSize(60, 60))
+            self.buttons.append(button)
+            button.clicked.connect(lambda name=name: self.
+                                   the_button_was_clicked(name))
+            grid.addWidget(button, *position)
 
-        for bt in btns2:
-            button = QPushButton(bt)
-            button.setFixedSize(QSize(60, 30))
-            button.clicked.connect(lambda: self.set_digit(bt))
-            layout4.addWidget(button)
+        self.move(1200, 550)
+        self.setWindowTitle('Calculator')
+        self.show()
 
-        for bt in btns3:
-            button = QPushButton(bt)
-            button.setFixedSize(QSize(60, 30))
-            button.clicked.connect(lambda: self.set_digit(bt))
-            layout5.addWidget(button)
-
-        for bt in btns4:
-            button = QPushButton(bt)
-            button.setFixedSize(QSize(60, 30))
-            button.clicked.connect(lambda: self.set_digit(bt))
-            layout6.addWidget(button)
-
-        for bt in btns5:
-            button = QPushButton(bt)
-            button.setFixedSize(QSize(60, 30))
-            button.clicked.connect(lambda: self.set_digit(bt))
-            layout7.addWidget(button)
-
-        btt = "111"
-        layout8.addWidget(QPushButton(btt))
-        button.setFixedSize(QSize(60, 30))
-        button.clicked.connect(lambda: self.set_digit(btt))
-
-        # layout = QVBoxLayout()
-        # layout.addWidget(self.input)
-        # layout.addWidget(self.label)
-
-        # layout.addWidget(self.button)
-        # self.button.clicked.connect(self.the_button_was_clicked)
-
-        layout1.addLayout(layout2)
-        layout1.addLayout(layout3)
-        layout1.addLayout(layout4)
-        layout1.addLayout(layout5)
-        layout1.addLayout(layout6)
-        layout1.addLayout(layout7)
-        layout1.addLayout(layout8)
-        container = QWidget()
-        container.setLayout(layout1)
-        self.setCentralWidget(container)
-
-    def set_digit(self, value):
-        print(value)
-        self.LCD.display(value)
-
-    def the_button_was_clicked(self):
-        # self.button.setText("Done")
+    def the_button_was_clicked(self, value):
         print("clicked")
+        self.LCD.display(value)
+        # print(button.name)
 
 
 app = QApplication(sys.argv)
 
 window = MainWindow()
 window.show()
-
 
 app.exec()
